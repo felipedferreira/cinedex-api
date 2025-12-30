e936a2b1a9e333c9064519861b8d337e1abd56bf
felipe d ferreira <felipedferreira11@gmail.com>
2025-12-30 14:42:28 -0500

    chore: updates CinedexWebApplicationFactory

--- tests/integration/Cinedex.Integration.Web/TestHelpers/CinedexWebApplicationFactory.cs
+++ tests/integration/Cinedex.Integration.Web/TestHelpers/CinedexWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cinedex.Web.IntegrationTests.TestHelpers;
@@ -11,6 +12,16 @@ public class CinedexWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        // Configure JWT secret for testing
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            var jwtSecret = Guid.NewGuid().ToString() + Guid.NewGuid().ToString(); // Generate a sufficiently long secret
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["Jwt:Secret"] = jwtSecret,
+            });
+        });
+
         builder.ConfigureServices(services =>
         {
             // Reconfigure antiforgery for test environment to allow HTTP requests
